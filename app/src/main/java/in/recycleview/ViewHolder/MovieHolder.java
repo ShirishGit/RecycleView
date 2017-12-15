4dfc5dae8ee6a649e1d0fc70a4a1d8b042edc87c
Shirish <shirishkulkarni92@gmail.com>
2017-12-15 10:42:16 +0530

    Created viewholder

---
+++ app/src/main/java/in/recycleview/ViewHolder/MovieHolder.java
@@ -0,0 +1,25 @@
+package in.recycleview.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import in.recycleview.R;
+
+/**
+ * Created by HP on 15-Dec-17.
+ */
+
+public class MovieHolder extends RecyclerView.ViewHolder {
+
+    public TextView title, year, genre;
+
+
+    public MovieHolder(View itemView) {
+        super(itemView);
+
+        title = itemView.findViewById(R.id.title);
+        year = itemView.findViewById(R.id.year);
+        genre = itemView.findViewById(R.id.genre);
+    }
+}
